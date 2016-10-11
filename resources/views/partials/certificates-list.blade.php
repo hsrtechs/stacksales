121ebf9a2cc00c1dccb8015f302309092153c060
Rishabh <jrishabh55@gmail.com>
2016-10-12 00:25:47 +0530

    Added Company Name in Table

--- resources/views/partials/certificates-list.blade.php
+++ resources/views/partials/certificates-list.blade.php
@@ -4,6 +4,7 @@
             <th>#IN</th>
             <th>Role</th>
             <th>Name</th>
+            <th>Company</th>
             <th>Issue Date</th>
             <th>Expiry Date</th>
         </tr>
@@ -14,6 +15,7 @@
                 <td>#{{ $certificate->in }}</td>
                 <td>{{ $certificate->role }}</td>
                 <td>{{ $certificate->name }}</td>
+                <td><a href="{{ route('Company.show',$certificate->Company->id) }}">{{ $certificate->Company->name }}</a></td>
                 <td>{{ $certificate->issue->toDateString() }}</td>
                 <td class="{{ ($certificate->expiry->diffInDays(\Carbon\Carbon::now()) <= 90 || $certificate->expiry->lt(\Carbon\Carbon::now())) ? 'text-danger' : '' }}">{{ $certificate->expiry->toDateString() }} ({{ $certificate->expiry->diffForHumans() }})</td>
             </tr>
